01883811f01193625605dcdb745aacc296de0c8c
notorious01 <tuborg1604@gmail.com>
2017-06-12 20:04:48 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,85 +1,31 @@
-#include <сomplex_t.hpp>
+#include <iteratorsort.hpp>
 #include <catch.hpp>
-SCENARIO("def constructor")
-{
-	complex_t Complex;
-	REQUIRE(Complex.real_() == 0);
-	REQUIRE(Complex.image_() == 0);
-}
 
-SCENARIO("param constructor")
+SCENARIO ("array")
 {
-	complex_t Complex(1, 2);
-	REQUIRE(Complex.real_() == 1);
-	REQUIRE(Complex.image_() == 2);
+  int a[] = {3, 2, 4, 5, 6};
+  int* s=a;
+  int* l=a+5;
+ insertion_sort(s,l);
+ REQUIRE(a[0] == 2);
+ REQUIRE(a[1] == 3);
+ REQUIRE(a[2] == 4);
+ REQUIRE(a[3] == 5);
+ REQUIRE(a[4] == 6);
 }
-
-SCENARIO("copy constructor")
+SCENARIO("matr")
 {
-	complex_t Complex(1, 2);
-	complex_t copy(Complex);
-	REQUIRE(copy.real_() == 1);
-	REQUIRE(copy.image_() == 2);
-}
-
-SCENARIO("operator *")
-{
-	complex_t m1(7, 6);
-	complex_t m2(5, 4);
-	complex_t m3(11, 58);
-	REQUIRE((m1*m2) == m3);
-}
-
-SCENARIO("operator /")
-{
-	complex_t m1(4, 5);
-	complex_t m2(4, 5);
-	complex_t m3(1, 0);
-	REQUIRE((m1 / m2) == m3);
-}
-
-SCENARIO("operator +=")
-{
-	complex_t m1(11, 1);
-	complex_t m2(8, 3);
-	complex_t m3(19, 4);
-	REQUIRE((m1 += m2) == m3);
-}
-
-SCENARIO("operator -=")
-{
-	complex_t m1(6, 10);
-	complex_t m2(3, 6);
-	complex_t m3(3, 4);
-	REQUIRE((m1 -= m2) == m3);
-}
-
-SCENARIO("operator *=")
-{
-	complex_t m1(7, 6);
-	complex_t m2(5, 4);
-	complex_t m3(11, 58);
-	REQUIRE((m1 *= m2) == m3);
-}
-
-SCENARIO("operator /=")
-{
-	complex_t m1(2, 1);
-	complex_t m2(2, 1);
-	complex_t m3(1, 0);
-	REQUIRE((m1 /= m2) == m3);
-}
-
-SCENARIO("operator =")
-{
-	complex_t m1(5, 7);
-	complex_t m2 = m1;
-	REQUIRE(m2 == m1);
-}
-
-SCENARIO("operator ==")
-{
-	complex_t m1(2, 5);
-	complex_t m2(2, 5);
-	REQUIRE(m1 == m2);
+  int arr[3][3] = {4, 21, 12, 5, 2, 7, 42, 26, 8};
+  int * beg = &arr[0][0];
+  int * end = &arr[2][3];
+  insertion_sort(beg,end);
+  REQUIRE(arr[0][0] == 2);
+  REQUIRE(arr[0][1] == 4);
+  REQUIRE(arr[0][2] == 5);
+  REQUIRE(arr[1][0] == 7);
+  REQUIRE(arr[1][1] == 8);
+  REQUIRE(arr[1][2] == 12);
+  REQUIRE(arr[2][0] == 21);
+  REQUIRE(arr[2][1] == 26);
+  REQUIRE(arr[2][2] == 42);
 }
